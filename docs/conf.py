7bb659af48c3c85d4f3f72fbddc1b95923e9c3ce
Sam Wu <sam.wu2@amd.com>
2023-10-19 09:49:59 -0600

    Import re for conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,8 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import re
+
 from rocm_docs import ROCmDocs
 
 with open('../CMakeLists.txt', encoding='utf-8') as f:
