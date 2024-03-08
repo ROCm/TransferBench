256dfdcde712cc9c983c72f4bdacd36957b8f4fe
Gilbert Lee <gilbert.lee@amd.com>
2024-03-08 21:53:32 +0000

    Enumerating missing DMA engines in topology display

--- src/include/EnvVars.hpp
+++ src/include/EnvVars.hpp
@@ -29,7 +29,7 @@ THE SOFTWARE.
 #include "Compatibility.hpp"
 #include "Kernels.hpp"
 
-#define TB_VERSION "1.48"
+#define TB_VERSION "1.49"
 
 extern char const MemTypeStr[];
 extern char const ExeTypeStr[];
