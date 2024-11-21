121e59b19acd2c8542614b498f4bb17d10ce55a1
akolliasAMD <akollias@amd.com>
2024-11-21 15:08:05 -0500

    adding default gfx targets

--- src/header/TransferBench.hpp
+++ src/header/TransferBench.hpp
@@ -419,7 +419,7 @@ namespace TransferBench
 //==========================================================================================
 
 // Macro for collecting CU/SM GFX kernel is running on
-#if defined(__gfx1100__) || defined(__gfx1101__) || defined(__gfx1102__)
+#if defined(__gfx1100__) || defined(__gfx1101__) || defined(__gfx1102__) || defined(__gfx1200__) || defined(__gfx1201__)
 #define GetHwId(hwId) hwId = 0
 #elif defined(__NVCC__)
 #define GetHwId(hwId) asm("mov.u32 %0, %smid;" : "=r"(hwId))
